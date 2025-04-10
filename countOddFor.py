961f0f3a6a62b632f86376443be00e4cf9c4ff50
lula-gif <lulittadesse789@gmail.com>
2025-04-10 03:10:15 +0300

    fizzbuzz

--- countOddFor.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        count = 0
-        for num in range(low, high+1):
-            if num%2 !=0:
-                count +=1
-        return count
-
-        
