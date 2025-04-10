4305706e31b2d0688d52de69fcd99400aac132dd
lula-gif <lulittadesse789@gmail.com>
2025-04-10 11:42:20 +0300

    reverseInteger.

--- reverseInteger.py
+++ reverseInteger.py
@@ -10,6 +10,10 @@ class Solution:
 
         if is_negative:
             reversed_num = -reversed_num
+        #If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+        if reversed_num < -2**31 or reversed_num > 2**31:
+            return 0
         
         return reversed_num
+        
         
